405e15443bb5892272b7829f0bcc319a45b38545
Ransford <dingyugege@sina.com>
2014-12-09 17:10:27 +0800

    optimize
    optimize

--- src/DFLClient.java
+++ src/DFLClient.java
@@ -1,12 +1,8 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
